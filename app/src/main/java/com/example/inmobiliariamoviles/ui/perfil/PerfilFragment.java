824b78292cadc76c5ac960118f2496bc27373cd0
Aladdin <ninjalover249@gmail.com>
2023-05-18 22:31:05 -0300

    Terminar fragment del perfil

--- app/src/main/java/com/example/inmobiliariamoviles/ui/perfil/PerfilFragment.java
+++ app/src/main/java/com/example/inmobiliariamoviles/ui/perfil/PerfilFragment.java
@@ -15,12 +15,50 @@ import com.example.inmobiliariamoviles.databinding.FragmentPerfilBinding;
 public class PerfilFragment extends Fragment {
 
     private FragmentPerfilBinding binding;
+    PerfilViewModel viewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        PerfilViewModel viewModel = new ViewModelProvider(this).get(PerfilViewModel.class);
+        viewModel = new ViewModelProvider(this).get(PerfilViewModel.class);
 
         binding = FragmentPerfilBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        viewModel.getPropietario().observe(getViewLifecycleOwner(), propietario -> {
+            binding.etId.setText(String.valueOf(propietario.getId()));
+            binding.etDni.setText(String.valueOf(propietario.getDni()));
+            binding.etNombre.setText(propietario.getNombre());
+            binding.etApellido.setText(propietario.getApellido());
+            binding.etEmail.setText(propietario.getEmail());
+            binding.etPass.setText(propietario.getContraseña());
+            binding.etTelefono.setText(propietario.getTelefono());
+        });
+
+        viewModel.getIsEdit().observe(getViewLifecycleOwner(), isEdit -> {
+            binding.etDni.setEnabled(isEdit);
+            binding.etNombre.setEnabled(isEdit);
+            binding.etApellido.setEnabled(isEdit);
+            binding.etEmail.setEnabled(isEdit);
+            binding.etPass.setEnabled(isEdit);
+            binding.etTelefono.setEnabled(isEdit);
+
+            binding.btEditar.setVisibility(isEdit ? View.GONE : View.VISIBLE);
+            binding.btGuardar.setVisibility(isEdit ? View.VISIBLE : View.GONE);
+        });
+
+        binding.btEditar.setOnClickListener(v -> {
+            viewModel.cambiarEstadoEdit();
+        });
+
+        binding.btGuardar.setOnClickListener(v -> {
+            viewModel.guardarPerfil(
+                    Long.parseLong(binding.etDni.getText().toString()),
+                    binding.etNombre.getText().toString(),
+                    binding.etApellido.getText().toString(),
+                    binding.etEmail.getText().toString(),
+                    binding.etPass.getText().toString(),
+                    binding.etTelefono.getText().toString()
+            );
+        });
 
         return root;
     }
--- app/src/main/java/com/example/inmobiliariamoviles/ui/perfil/PerfilViewModel.java
+++ app/src/main/java/com/example/inmobiliariamoviles/ui/perfil/PerfilViewModel.java
@@ -1,10 +1,66 @@
 package com.example.inmobiliariamoviles.ui.perfil;
 
+import android.app.Application;
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class PerfilViewModel extends ViewModel {
+import com.example.inmobiliariamoviles.models.Propietario;
+import com.example.inmobiliariamoviles.request.ApiClient;
 
-    public PerfilViewModel() {}
+public class PerfilViewModel extends AndroidViewModel {
+
+    private Context context;
+    private MutableLiveData<Propietario> propietario;
+    private MutableLiveData<Boolean> isEdit;
+    ApiClient apiClient;
+
+    public PerfilViewModel(@NonNull Application application) {
+        super(application);
+        this.context = application.getApplicationContext();
+
+        apiClient = ApiClient.getApi();
+
+        propietario = new MutableLiveData<>();
+        cargarDatosUsuario();
+    }
+
+    public LiveData<Propietario> getPropietario() { return propietario; }
+
+    public LiveData<Boolean> getIsEdit() {
+        if (isEdit == null) {
+            isEdit = new MutableLiveData<>();
+            isEdit.setValue(false);
+        }
+        return isEdit;
+    }
+
+    public void cargarDatosUsuario() {
+        Propietario usuario = apiClient.obtenerUsuarioActual();
+
+        if (usuario == null) {
+            Toast.makeText(context, "Error al obtener usuario", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        propietario.setValue(usuario);
+    }
+
+    public void cambiarEstadoEdit() {
+        isEdit.setValue(!isEdit.getValue());
+    }
+
+    public void guardarPerfil(Long dni, String nombre, String apellido, String email, String contrasena, String telefono) {
+        Propietario usuarioActual = new Propietario(propietario.getValue().getId(), dni, nombre, apellido, email, contrasena, telefono, propietario.getValue().getAvatar());
+        apiClient.actualizarPerfil(usuarioActual);
+        propietario.setValue(usuarioActual);
+        cambiarEstadoEdit();
+    }
+
+
 }
